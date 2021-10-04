ee5dc4039dcd5eda1689491c783227cf5263993d
Caglar DURMAZ <caglar.durmaz@vodafone.com>
2021-10-04 18:54:59 +0300

    Spring Context

---
+++ src/main/java/demo/springframework/sfgdi/controllers/MyController.java
@@ -0,0 +1,13 @@
+package demo.springframework.sfgdi.controllers;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyController {
+
+    public String sayHello(){
+        System.out.println("Hello World!!!");
+
+        return "Hi Folks!!";
+    }
+}
