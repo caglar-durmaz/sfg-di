6871cc089c944733ba1cf747b6dd96224b4cd373
Caglar DURMAZ <caglar.durmaz@vodafone.com>
2021-10-06 20:33:56 +0300

    Dependency Injection using Spring Framework

--- src/main/java/demo/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/demo/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,7 +1,9 @@
 package demo.springframework.sfgdi.controllers;
 
 import demo.springframework.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
     private final GreetingService greetingService;
--- src/main/java/demo/springframework/sfgdi/controllers/PropertyInjectedController.java
+++ src/main/java/demo/springframework/sfgdi/controllers/PropertyInjectedController.java
@@ -1,9 +1,13 @@
 package demo.springframework.sfgdi.controllers;
 
 import demo.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController {
 
+    @Autowired
     public GreetingService greetingService;
 
     public String getGreeting(){
