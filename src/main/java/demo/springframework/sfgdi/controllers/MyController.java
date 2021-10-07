db27e8c7d615388df916102ae3ab9c9073ea3a76
Caglar DURMAZ <caglar.durmaz@vodafone.com>
2021-10-07 20:48:35 +0300

    Primary Beans

--- src/main/java/demo/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/demo/springframework/sfgdi/controllers/MyController.java
@@ -1,13 +1,18 @@
 package demo.springframework.sfgdi.controllers;
 
+import demo.springframework.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyController {
 
-    public String sayHello(){
-        System.out.println("Hello World!!!");
+    private final GreetingService greetingService;
 
-        return "Hi Folks!!";
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
     }
 }
