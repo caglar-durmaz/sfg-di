52211e7766a64a3db67c64a0ca9e536e2f8b3671
kafeincdurmaz <caglar.durmaz@vodafone.com>
2021-11-11 20:20:51 +0300

    assignment fixes

--- src/main/java/demo/springframework/sfgdi/services/CatPetService.java
+++ src/main/java/demo/springframework/sfgdi/services/CatPetService.java
@@ -3,8 +3,9 @@ package demo.springframework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+
+@Service
 @Profile("cat")
-@Service("petService")
 public class CatPetService implements PetService{
 
     @Override
